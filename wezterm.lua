ef975f03117ee72b31b89bc29c08656ba906d13d
TheLeoP <eugenio2305@hotmail.com>
2024-03-29 22:21:31 -0500

    Use zsh on Linux

--- wezterm.lua
+++ wezterm.lua
@@ -3,12 +3,18 @@ local config = wezterm.config_builder()
 
 local mux = wezterm.mux
 
+local is_windows = function() return wezterm.target_triple:find "windows" end
+
 wezterm.on("gui-startup", function()
   local tab, pane, window = mux.spawn_window {}
   window:gui_window():maximize()
 end)
 
-config.default_prog = { "pwsh" }
+if is_windows() then
+  config.default_prog = { "pwsh" }
+else
+  config.default_prog = { "zsh" }
+end
 config.color_scheme = "Gruvbox Dark (Gogh)"
 config.prefer_egl = true
 
